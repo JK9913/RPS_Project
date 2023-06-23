b247923c9446a069cb1de77f0906ec7b8d75f6f5
Jan-Kristian Karlsen <jan-kristiankarlsen@jankristiansmbp.lan>
2023-06-23 21:36:10 +0200

    Reowrk of rock paper scissors game with a UI consisting of 3 buttons, scoreboard and announcement text

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputuerChoice(){
-    let randomNumber = Math.floor(Math.random() * (3 - 1 +1)) + 1;
+    const randomNumber = Math.floor(Math.random() * (3 - 1 +1)) + 1;
 
     switch (randomNumber) {
         case 1:
@@ -9,96 +9,109 @@ function getComputuerChoice(){
             return 'Paper';
             break;
         case 3:
-            return 'Scissor';
+            return 'Scissors';
             break;
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    console.log(playerSelection);
     playerSelection = playerSelection.toLowerCase();
+    let computerSelection = getComputuerChoice();
     computerSelection = computerSelection.toLowerCase();
     
+    // Select the elements that needs to change text
+    const winnerText = document.querySelector('.winner_text');
+    const results = document.querySelector('.result_text');
+    const scorePlayer = document.querySelector('.result_player');
+    const scoreComputer = document.querySelector('.result_computer');
+
+
+    //initiate variables
+    let decideRound = ""
     if (playerSelection.toLowerCase() === 'rock') {
         switch (computerSelection.toLowerCase()) {
             case 'rock':
-                return 'Both chose ' + playerSelection + ', it\'s a tie';
+                decideRound = 'Both chose ' + playerSelection + ', it\'s a tie';
+                winnerText.textContent = decideRound 
                 break; 
-            case 'scissor':
-                return 'Computer chose ' + computerSelection + ', Player wins!';
+            case 'scissors':
+                decideRound = 'Computer chose ' + computerSelection + ', Player wins!';
+                winnerText.textContent = decideRound
+                scorePlayer.textContent = (parseInt(scorePlayer.textContent) + 1).toString()
                 break;
             case 'paper':
-                return 'Computer chose ' + computerSelection + ', Computer wins!';
+                decideRound = 'Computer chose ' + computerSelection + ', Computer wins!';
+                winnerText.textContent = decideRound
+                scoreComputer.textContent = (parseInt(scoreComputer.textContent) + 1).toString()
                 break;
             default:
-                return 'Could not recognize computer choice...'
+                decideRound = 'Could not recognize computer choice...'
         }
     }else if (playerSelection.toLowerCase() === 'paper'){
         switch (computerSelection.toLowerCase()) {
             case 'paper':
-                return 'Both chose ' + playerSelection + ', it\'s a tie';
+                decideRound = 'Both chose ' + playerSelection + ', it\'s a tie';
                 break; 
             case 'rock':
-                return 'Computer chose ' + computerSelection + ', Player wins!';
+                decideRound = 'Computer chose ' + computerSelection + ', Player wins!';
+                winnerText.textContent = decideRound 
+                scorePlayer.textContent = (parseInt(scorePlayer.textContent) + 1).toString()
                 break;
-            case 'scissor':
-                return 'Computer chose ' + computerSelection + ', Computer wins!';
+            case 'scissors':
+                decideRound = 'Computer chose ' + computerSelection + ', Computer wins!';
+                winnerText.textContent = decideRound
+                scoreComputer.textContent = (parseInt(scoreComputer.textContent) + 1).toString()
                 break;
             default:
-                return 'Could not recognize computer choice...'
+                decideRound = 'Could not recognize computer choice...'
         }
-    }else if  (playerSelection.toLowerCase() === 'scissor'){
+    }else if  (playerSelection.toLowerCase() === 'scissors'){
         switch (computerSelection.toLowerCase()) {
-            case 'scissor':
-                return 'Both chose ' + playerSelection + ', it\'s a tie';
+            case 'scissors':
+                decideRound = 'Both chose ' + playerSelection + ', it\'s a tie';
                 break; 
             case 'paper':
-                return 'Computer chose ' + computerSelection + ', Player wins!';
+                decideRound = 'Computer chose ' + computerSelection + ', Player wins!';
+                winnerText.textContent = decideRound
+                scorePlayer.textContent = (parseInt(scorePlayer.textContent) + 1).toString()
                 break;
             case 'rock':
-                return 'Computer chose ' + computerSelection + ', Computer wins!';
+                decideRound = 'Computer chose ' + computerSelection + ', Computer wins!';
+                winnerText.textContent = decideRound 
+                scoreComputer.textContent = (parseInt(scoreComputer.textContent) + 1).toString()
                 break;
             default:
-                return 'Could not recognize computer choice...'
+                decideRound = 'Could not recognize computer choice...'
         }
     }else {
-        return 'Couldn\'t recognize user input, please try again.'
+        decideRound = 'Couldn\'t recognize user input, please try again.'
     }
 }
+
+function playerSelection() {
+    let playerSelect = prompt('Please enter rock, paper, or scissor...');
+        while (!(arrayChoices.includes(playerSelect))) {
+            playerSelect = prompt('Invalid entry \n Please enter rock, paper, or scissor...');
+        };
+    console.log(playerSelect);
+    return playerSelect;
+}
+
 
 function game() {
     let scorePlayer = 0;
     let scoreComputer = 0;
 
-    for (let i = 1; i <= 5; i++) {
-        const arrayChoices = ['rock','paper','scissor'];
-        let playerSelect = prompt('Please enter rock, paper, or scissor...');
-        while (!(arrayChoices.includes(playerSelect))) {
-            playerSelect = prompt('Invalid entry \n Please enter rock, paper, or scissor...');
-        }
 
-        let computerSelect = getComputuerChoice();
-
-        let result = playRound(playerSelect, computerSelect);
-        result = result.toLowerCase();
-        console.log(result);
-
-        if (result.includes('player wins!')) {
-            scorePlayer++; 
-        }else if (result.includes('computer wins!')) {
-            scoreComputer++
-        }else {
-            i--;
-        }
-    }
-
-    if (scorePlayer > scoreComputer) {
-        return ('Player: ' + scorePlayer + '\n Computer: ' + scoreComputer + '\n Player wins the game!' )
-    }else {
-        return ('Player: ' + scorePlayer + '\n Computer: ' + scoreComputer + '\n Computer wins the game!' )
-    }
 
 }
 
-let winner = game();
-alert(winner);
 
+
+const buttons  = document.querySelectorAll('button');
+
+// For each method to iterate through and add eventListener
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => playRound(btn.textContent))
+})
